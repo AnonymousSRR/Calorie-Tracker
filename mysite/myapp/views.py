58058ba1f520fd583f57ec7b59566de0c2b4ecf7
Sahil <sahilrenapurkar710@gmail.com>
2024-06-02 13:30:04 +0530

    Authentication work done

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import redirect, render
 from .models import Food, Consume
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'myapp/signup.html', {'form' : form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('index')  
+    else:
+        form = AuthenticationForm()  
+    return render(request, 'myapp/login.html', {'form' : form})
+
 def index(request):
     if request.method == "POST":
         food_consumed = request.POST['food_consumed']
@@ -12,7 +35,7 @@ def index(request):
         foods = Food.objects.all()
     
     else:    
-        foods = Food.objects.all()
+        foods = Food.objects.all(), 
         
     consumed_food = Consume.objects.filter(user = request.user)
     return render(request, 'myapp/index.html', {'foods':foods, 'consumed_food':consumed_food})
